19d56fcd0d8ce58daf793c41a65c0553e5fb090b
jong <jongartman@gmail.com>
2013-08-28 11:38:08 -0500

    Remove unecessary methods from base mongo entity.
    - Simplify entity tests.

--- tests/test_entities.py
+++ tests/test_entities.py
@@ -1,33 +1,20 @@
 from springfield_mongo.entities import Entity as MongoEntity
 from springfield_mongo import fields
 from springfield_mongo import utils
+import pytest
 from bson.objectid import ObjectId
+from bson.objectid import InvalidId
 
 
 def test_mongo_entity():
+    """
+    Verify that a MongoEntity has the attributes and attribute types that we expect.
+    """
     m = MongoEntity()
 
-    m.id = ObjectId()
-
-    # verify that when converting to json, the 'id' key is preserved.
-    m_json = m.jsonify()
-    assert 'id' in m_json
-
-    # ...but verify that when converting to a mongo document, the id is 
-    # translated.
-    m_mongo = utils.entity_to_mongo(m)
-    assert '_id' in m_mongo
-
-    # verify that 'something' trying to set the invalid '_id' key will be
-    # translated into the 'id' field.
     i = ObjectId()
-    m['_id'] = i
-    assert not hasattr(m, '_id')
+    m.id = i
     assert m.id == i
 
-    # verify that __setattr__ behaves the same as __setitem__ with regard
-    # to the translated '_id' key.
-    j = ObjectId()
-    m._id = j
-    assert not hasattr(m, '_id')
-    assert m.id == j
+    with pytest.raises(InvalidId):
+        m.id = 'monkey'
