104d447420cb1b4e4cab18a6b787b3323bbfc4ce
Mike Thornton <msthornton@gmail.com>
2014-02-21 23:55:41 -0600

    Handle FlexEntity with entity_from_mongo

--- springfield_mongo/utils.py
+++ springfield_mongo/utils.py
@@ -1,44 +1,48 @@
-from springfield import Entity
+from springfield import Entity, FlexEntity
 
 
 def entity_from_mongo(cls, values):
     """
     Construct an Entity of type `cls` from a Mongo document.
     """
-    entity = cls()
-    if hasattr(values, '__values__'):
-        for key, val in values.__values__.items():
-            if key == '_id':
-                key = 'id'
+    if values:
+        entity = cls()
+        if hasattr(values, '__values__'):
+            for key, val in values.__values__.items():
+                if key == '_id':
+                    key = 'id'
 
-            if key in entity.__fields__:
-                entity.set(key, val)
-    else:
-        for key, val in values.items():
-            if key == '_id':
-                key = 'id'
+                if key in entity.__fields__:
+                    entity.set(key, val)
+        else:
+            for key, val in values.items():
+                if key == '_id':
+                    key = 'id'
 
-            if key in entity.__fields__:
-                entity.set(key, val)
-    return entity
+                if key in entity.__fields__:
+                    entity.set(key, val)
+        return entity
 
 
 def entity_to_mongo(entity):
     """
     Convert an Entity type into a structure able to be stored in Mongo.
     """
-    data = {}
-    for key, val in entity.__values__.iteritems():
-        field = entity.__fields__[key]
+    if entity:
+        data = {}
+        for key, val in entity.__values__.iteritems():
+            field = entity.__fields__[key]
 
-        if isinstance(val, Entity):
-            val = entity_to_mongo(val)
-        else:
-            val = field.flatten(val)
+            if isinstance(val, FlexEntity):
+                val = val.flatten()
+            elif isinstance(val, Entity):
+                val = entity_to_mongo(val)
+            else:
+                val = field.flatten(val)
 
-        if key == 'id':
-            key = '_id'
+            if key == 'id':
+                key = '_id'
 
-        data[key] = val
+            data[key] = val
 
-    return data
+        return data
